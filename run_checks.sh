a1bafefa4793d90a90b707575c6cca6492c1c343
gizmo385 <gizmo385@users.noreply.github.com>
2024-08-21 22:29:23 +0000

    Use more new features in uv

--- run_checks.sh
+++ run_checks.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-source ./.venv/bin/activate
-uv run --extra=checks pyright
-uv run --extra=checks ruff check
+uvx ruff check
--- start.sh
+++ start.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-uv venv --quiet
-uv pip sync --quiet requirements.txt
-.venv/bin/python -m lazy_github
+uv run lazy_github/__main__.py
