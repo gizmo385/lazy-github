275ac682b2a7fbaaeb4acc0eaa7788dba43f75fe
gizmo385 <gizmo385@users.noreply.github.com>
2024-12-01 08:09:52 +0000

    Whoops I'm dumb

--- lazy_github/lib/github_cli.py
+++ lazy_github/lib/github_cli.py
@@ -1,6 +1,5 @@
 import json
 import subprocess
-import time
 from asyncio import sleep
 from dataclasses import dataclass
 
@@ -22,7 +21,7 @@ async def _run_gh_cli_command(command: list[str]) -> _FinishedCommand:
     full_command = ["gh"] + command
     proc = subprocess.Popen(full_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     while True:
-        if proc.poll() is not None:
+        if proc.poll() is None:
             raw_stdout, raw_stderr = proc.communicate()
             return _FinishedCommand(proc.returncode, raw_stdout.decode(), raw_stderr.decode())
         else:
