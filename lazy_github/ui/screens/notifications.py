52ae7f494e1485e11b60b942879a935da668f25c
gizmo385 <gizmo385@users.noreply.github.com>
2024-12-23 01:26:14 -0700

    Fix import sorting

--- lazy_github/ui/screens/notifications.py
+++ lazy_github/ui/screens/notifications.py
@@ -5,10 +5,10 @@ from textual.coordinate import Coordinate
 from textual.screen import ModalScreen
 from textual.widgets import DataTable, Markdown, TabbedContent, TabPane
 
-from lazy_github.lib.messages import NotificationMarkedAsRead, NotificationSelected
 from lazy_github.lib.bindings import LazyGithubBindings
 from lazy_github.lib.constants import BULLET_POINT, CHECKMARK
 from lazy_github.lib.github.notifications import fetch_notifications, mark_notification_as_read
+from lazy_github.lib.messages import NotificationMarkedAsRead, NotificationSelected
 from lazy_github.models.github import Notification
 from lazy_github.ui.widgets.common import LazyGithubFooter, SearchableDataTable
 
